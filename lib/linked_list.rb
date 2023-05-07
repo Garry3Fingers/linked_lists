0d02ec85502a4c110735c847e37534257afe6aa7
Garry3Fingers <www.vlad78@yandex.ru>
2023-05-07 15:46:36 +0300

    Create head, tail, prepend, append methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,9 +2,26 @@
 
 # This class represents the full list
 class LinkedList
+  attr_accessor :head, :tail
+
   def initialize
     @head = nil
     @tail = nil
+  end
+
+  def append(value)
+    if @head.nil?
+      prepend(value)
+    else
+      tmp = @head
+      tmp = tmp.next_node until tmp.next_node.nil?
+      tmp.next_node = Node.new(value)
+      @tail = tmp.next_node
+    end
+  end
+
+  def prepend(value)
+    @head = Node.new(value)
   end
 end
 
@@ -17,3 +34,14 @@ class Node
     @next_node = next_node
   end
 end
+
+linked_list = LinkedList.new
+linked_list.prepend({ name: 'Valdislav', age: 29 })
+# p linked_list.head.next_node
+linked_list.append({ name: 'Anastasia', age: 24 })
+# puts linked_list.tail.value[:name]
+# p linked_list.tail.next_node
+linked_list.append({ name: 'Aleksandr', age: 30 })
+puts linked_list.tail.value[:name]
+p linked_list.tail.next_node
+puts linked_list.head.next_node.value[:name]
